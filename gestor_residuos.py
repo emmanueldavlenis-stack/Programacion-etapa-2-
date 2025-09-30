e0af0a345499e3f91edba549af07c193d7c77239
Julian Vega <pamilo1@hotmail.com>
2025-09-30 16:49:04 -0500

    Gestor updated

--- gestor_residuos.py
+++ gestor_residuos.py
@@ -6,11 +6,20 @@ metodos_tratamiento = {1: "Reciclaje", 2: "Incineración", 3: "Compostaje", 4: "
 
 class Residuo():
     def __init__(self, nombre, tipo, tratamiento, peso, recuperacion):
+        assert isinstance(nombre, str) and nombre.strip(), "El texto debe ser un nombre válido."
+        assert isinstance(tipo, int) and tipo in tipos_residuos, "Ese tipo de residuo no existe."
+        assert isinstance(tratamiento, int) and tratamiento in metodos_tratamiento, "Ese método de tratamiento no existe."
+        assert isinstance(peso, int) and peso > 0, "Peso inválido. Debe ser un número entero positivo"
+        assert isinstance(recuperacion, float) and 0 <= recuperacion <= 100, "Porcentaje inválido. Debe ser un numero entre 0 y 100"
         self.nombre = nombre
-        self.tipo = tipo
-        self.tratamiento = tratamiento,
+        self.tipo = tipos_residuos[tipo]
+        self.tratamiento = metodos_tratamiento[tratamiento]
         self.peso = peso
         self.recuperacion = recuperacion
+
+    def __str__(self):
+        return (f"Nombre: {self.nombre} | Tipo: {self.tipo} | Método de tratamiento: {self.tratamiento} | "
+                f"Peso: {self.peso}kg | Porcentaje de recuperacion: {self.recuperacion:.1f}%")
 
 class GestorResiduos:
     def __init__(self):
@@ -28,13 +37,32 @@ class GestorResiduos:
         tratamiento = options(*metodos_tratamiento.values())
         print(default_text("Peso en kilogramos (sin kg):"))
         peso = input(INPUT_CHAR + " ")
-        print(default_text("Porcentaje de recuperación estimado:"))
+        print(default_text("Porcentaje de recuperación estimado (sin %):"))
         recuperacion = input(INPUT_CHAR + " ")
 
         try:
-            residuo = Residuo(nombre, tipo, tratamiento, int(peso), float(recuperacion))
+            residuo = Residuo(nombre, int(tipo), int(tratamiento), int(peso), float(recuperacion))
             self.residuos.append(residuo)
             print()
             print(default_text("Residuo sólido registrado correctamente"))
+            input("\nPresione enter para continuar...")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def listar_tipo(self):
+        print(default_text("Tipo de residuo:"))
+        try:
+            tipo = int(options(*tipos_residuos.values()))
+            if not tipo in tipos_residuos:
+                print("Opción inválida, inténtelo de nuevo")
+                return
+            tipo = tipos_residuos[tipo]
+            print(separator())
+            print(title_style(f"Lista de residuos de tipo {tipo}"))
+            print(separator())
+            filtrado = [residuo for residuo in self.residuos if residuo.tipo == tipo]
+            for i, residuo in enumerate(filtrado):
+                print(f"{i + 1}. {residuo}")
+            input("\nPresione enter para continuar...")
         except Exception as e:
             print(f"Error: {e}")
--- main.py
+++ main.py
@@ -3,39 +3,40 @@ from gestor_residuos import GestorResiduos
 
 def main():
     gestor = GestorResiduos()
-    print(separator())
-    print(title_style("MENU PRINCIPAL"))
-    print(separator())
-    option = options(
-        "Registrar residuo",
-        "Listar residuos por tipo",
-        "Listar residuos por método de tratamiento",
-        "Generar resumen estadístico",
-        "Exportar resumen (txt/pdf)",
-        "Generar CSV con residuos simulados",
-        "Cargar residuos desde CSV",
-        "Exportar reporte profesional (PDF)",
-        "Salir"
-    )
-    match option:
-        case "1":
-            gestor.registrar_residuo()
-        case "2":
-            pass
-        case "3":
-            pass
-        case "4":
-            pass
-        case "5":
-            pass
-        case "6":
-            pass
-        case "7":
-            pass
-        case "8":
-            pass
-        case "9":
-            pass
+    while True:
+        print(separator())
+        print(title_style("MENU PRINCIPAL"))
+        print(separator())
+        option = options(
+            "Registrar residuo",
+            "Listar residuos por tipo",
+            "Listar residuos por método de tratamiento",
+            "Generar resumen estadístico",
+            "Exportar resumen (txt/pdf)",
+            "Generar CSV con residuos simulados",
+            "Cargar residuos desde CSV",
+            "Exportar reporte profesional (PDF)",
+            "Salir"
+        )
+        match option:
+            case "1":
+                gestor.registrar_residuo()
+            case "2":
+                gestor.listar_tipo()
+            case "3":
+                pass
+            case "4":
+                pass
+            case "5":
+                pass
+            case "6":
+                pass
+            case "7":
+                pass
+            case "8":
+                pass
+            case "9":
+                break
 
 if __name__ == "__main__":
     main()
